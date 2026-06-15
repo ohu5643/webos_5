b1a1ee8b9303476cc6a0a6f3737af3fd9418c046
ohu5643 <166441068+ohu5643@users.noreply.github.com>
2026-06-15 13:23:16 +0900

    Update CommandParser.js

--- src/services/CommandParser.js
+++ src/services/CommandParser.js
@@ -63,6 +63,36 @@ export function parseCommand(text){
 
     }
 
+    if(
+    lower.includes("내 파일")
+    ||
+    lower.includes("파일 목록")
+){
+    return {
+        action:"list_files"
+    };
+}
+
+
+if(
+    lower.includes("운영체제 기능")
+    ||
+    lower.includes("os 기능")
+){
+    return {
+        action:"show_os_features"
+    };
+}
+
+
+if(
+    lower.includes("터미널 명령어")
+){
+    return {
+        action:"show_terminal_commands"
+    };
+}
+
 
     return null;
 
